6da5bd0b05c8056cd8e64ad7bd6474f0d8d439eb
Joseph George <febafebiej@gmail.com>
2026-06-02 20:19:34 +0530

    now

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+import { setHeapSnapshotNearHeapLimit } from 'node:v8';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -20,3 +21,14 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+
+//steps
+//git clone URL
+//untracked
+// git add - workspace
+//git commit - to local repository(git commit -m "description for commit")
+// git push - to remote repository(github)
+//git pull - from remote repository files to get here
+//git checkout branchname - to switch to other branches
+
